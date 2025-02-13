52355bc0786694d65a7a96a777da8ab8f08b938b
jassary <jassaryespinoza@gmail.com>
2025-02-13 16:06:27 -0600

    agregando un sistema de contraseña

--- practica.py
+++ practica.py
@@ -1,1 +1,11 @@
-print("hola mundo"
+#SISTEMA DE CONTRASEÑA
+
+nombre_del_usuario="jassary espinoza amador"
+contraseña_correcta="jass123"
+usuario=input("ingrese su usuario: ")
+contraseña=input("ingrese su contraseña: ")
+if usuario == nombre_del_usuario and contraseña == contraseña_correcta:
+    print("acceso concedido")
+else:
+    print("acceso denegado")
+    
